be6710d15016634b31cdb5184c47dfd38c72b07e
Alen Sugimoto <alensugimoto@gmail.com>
2021-06-05 15:50:37 +0200

    Complete task 2

--- src/CardsContainer.js
+++ src/CardsContainer.js
@@ -1,17 +1,39 @@
+import { IconButton, Input } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
+import { useState } from "react";
 import MyCard from "./MyCard";
 
-const CardsContainer = ({ selected, cardsText }) => {
-  const listItems = cardsText.map((value, index) => (
-    <li>
-      <MyCard value={value} highlight={index === selected} />
-    </li>
-  ));
+function CardsContainer(props) {
+  const [selected, setSelected] = useState(props.selected);
+  const [cardsText, setCardsText] = useState(props.cardsText);
+
+  const handleClick = () => {
+    const newCardsText = [...cardsText, ""];
+    setCardsText(newCardsText);
+  };
+
+  const handleChange = (event) => {
+    const newSelected = Number(event.target.value);
+    setSelected(newSelected);
+  };
 
   return (
     <div className="CardsContainer">
-      <ol>{listItems}</ol>
+      <IconButton onClick={handleClick}>
+        <Add />
+      </IconButton>
+      <form>
+        <Input type="number" defaultValue={selected} onChange={handleChange} />
+      </form>
+      <ol>
+        {cardsText.map((value, index) => (
+          <li key={index}>
+            <MyCard value={value} highlight={index === selected} />
+          </li>
+        ))}
+      </ol>
     </div>
   );
-};
+}
 
 export default CardsContainer;
--- src/MyCard.js
+++ src/MyCard.js
@@ -1,13 +1,28 @@
-import { Box } from "@material-ui/core";
+import { TextField } from "@material-ui/core";
+import { useState } from "react";
 
-const MyCard = ({ value, highlight }) => {
+function MyCard(props) {
+  const highlight = props.highlight;
+  const [value, setValue] = useState(props.value);
+
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+  };
+
   return (
     <div className="MyCard">
-      <Box border={highlight ? 1 : 0} borderColor="red">
-        {value}
-      </Box>
+      <form>
+        <TextField
+          type="text"
+          variant="outlined"
+          error={highlight}
+          defaultValue={value}
+          onChange={handleChange}
+        />
+      </form>
     </div>
   );
-};
+}
 
 export default MyCard;
